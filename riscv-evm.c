6bbd1d6181708f541ebda68a446cd124853d09cc
Timofey Titovets <nefelim4ag@gmail.com>
2026-05-11 02:08:43 +0200

    Compressed decoder size optimizations
    Signed-off-by: Timofey Titovets <nefelim4ag@gmail.com>

--- riscv-evm.c
+++ riscv-evm.c
@@ -434,56 +434,43 @@ ecall(uint8_t *PC, uint32_t *X, uint32_t inst, uint8_t MODE)
 static uint8_t *
 c0(uint8_t *PC, uint32_t *X, uint16_t inst, uint8_t MODE) {
     const uint8_t opcode = inst >> 13;
-    uint32_t *X8 = &X[8];
-    uint32_t imm;
-    uint8_t rd8;
-    uint8_t rs18;
-    uint8_t *mem;
+    uint8_t rd = ((inst >> 2) & 0x7) + 8;
+    uint8_t rs1 = ((inst >> 7) & 0x7) + 8;
+    uint32_t imm = ((inst >> 6) & 0x1) << 2 |
+                   ((inst >> 10) & 0x7) << 3 |
+                   ((inst >> 5) & 0x1) << 6;
+    uint8_t *mem = (uint8_t *) (X[rs1] + imm);
     switch (opcode) {
         case 0: // c.addi4spn
-            rd8 = (inst >> 2) & 0x7;
             imm = ((inst >> 6) & 0x1) << 2;
             imm |= ((inst >> 5) & 0x1) << 3;
             imm |= ((inst >> 11) & 0x3) << 4;
             imm |= ((inst >> 7) & 0xf) << 6;
             if (MODE)
-                evm_print("c.addi4spn x[%d] = X[2] + %d\n", 8+rd8,
-                    imm);
+                evm_print("c.addi4spn x[%d] = X[2] + %d\n", rd, imm);
             if (MODE == EVM_MODE_DISASM)
                 break;
-            X8[rd8] = X[2] + imm;
+            X[rd] = X[2] + imm;
             break;
         case 2: // c.lw
-            rs18 = (inst >> 7) & 0x7;
-            rd8 = (inst >> 2) & 0x7;
-            imm = ((inst >> 6) & 0x1) << 2;
-            imm |= ((inst >> 10) & 0x7) << 3;
-            imm |= ((inst >> 5) & 0x1) << 6;
             if (MODE)
-                evm_print("c.lw x[%d] = M[x[%d] + %d]\n", 8 + rd8,
-                    8 + rs18, imm);
+                evm_print("c.lw x[%d] = M[x[%d] + %d]\n", rd, rs1, imm);
             if (MODE == EVM_MODE_DISASM)
                 break;
-            mem = (uint8_t *) (X8[rs18] + imm);
             uint32_t val = 0;
             for (int i = 0; i < 4; i++)
                 val |= mem[i] << (i * 8);
-            X8[rd8] = val;
+            X[rd] = val;
             break;
         case 6: // c.sw
-            rs18 = (inst >> 7) & 0x7;
-            uint8_t rs28 = (inst >> 2) & 0x7;
-            imm = ((inst >> 6) & 0x1) << 2;
-            imm |= ((inst >> 10) & 0x7) << 3;
-            imm |= ((inst >> 5) & 0x1) << 6;
+            uint8_t rs2 = rd;
             if (MODE)
-                evm_print("c.sw M[x[%d] + %d] = x[%d]\n", 8 + rs18,
-                    imm, 8 + rs28);
+                evm_print("c.sw M[x[%d] + %d] = x[%d]\n", rs1,
+                    imm, rs2);
             if (MODE == EVM_MODE_DISASM)
                 break;
-            mem = (uint8_t *) (X8[rs18] + imm);
             for (int i = 0; i < 4; i++)
-                mem[i] = X8[rs28] >> (i * 8);
+                mem[i] = X[rs2] >> (i * 8);
             break;
         default:
             if (MODE)
@@ -495,15 +482,12 @@ c0(uint8_t *PC, uint32_t *X, uint16_t inst, uint8_t MODE) {
 static uint8_t *
 c1(uint8_t *PC, uint32_t *X, uint16_t inst, uint8_t MODE) {
     const uint8_t opcode = inst >> 13;
-    uint32_t *X8 = &X[8];
-    uint8_t rd;
-    uint32_t imm;
+    uint8_t rd = (inst >> 7) & 0x1f;
+    uint32_t imm = (inst >> 2) & 0x1f;
     uint32_t offset;
-    uint8_t rs18;
+    uint8_t rs1;
     switch (opcode) {
         case 0: // c.addi
-            rd = (inst >> 7) & 0x1f;
-            imm = (inst >> 2) & 0x1f;
             if (inst & 0x1000)
                 imm |= 0xffffffe0;
             if (MODE)
@@ -512,30 +496,7 @@ c1(uint8_t *PC, uint32_t *X, uint16_t inst, uint8_t MODE) {
                 break;
             X[rd] += imm;
             break;
-        case 1: // c.jal
-            offset =
-                ((inst >> 8)  & 0x1) << 10 |
-                ((inst >> 10) & 0x1) << 9  |
-                ((inst >> 9)  & 0x1) << 8  |
-                ((inst >> 6)  & 0x1) << 7  |
-                ((inst >> 7)  & 0x1) << 6  |
-                ((inst >> 2)  & 0x1) << 5  |
-                ((inst >> 11) & 0x1) << 4  |
-                ((inst >> 5)  & 0x1) << 3  |
-                ((inst >> 4)  & 0x1) << 2  |
-                ((inst >> 3)  & 0x1) << 1;
-            if (inst & 0x1000)
-                offset |= 0xfffff800;
-            if (MODE)
-                evm_print("c.jal x[1] = pc+2; pc += sext(%d)\n",
-                    (int32_t)offset);
-            if (MODE == EVM_MODE_DISASM)
-                break;
-            X[1] = (uint32_t) PC + 2;
-            return PC + offset;
         case 2: // c.li
-            rd = (inst >> 7) & 0x1f;
-            imm = (inst >> 2) & 0x1f;
             if (inst & 0x1000)
                 imm |= 0xffffffe0;
             if (MODE)
@@ -545,7 +506,6 @@ c1(uint8_t *PC, uint32_t *X, uint16_t inst, uint8_t MODE) {
             X[rd] = imm;
             break;
         case 3:
-            rd = (inst >> 7) & 0x1f;
             if (rd == 2) { // c.addi16sp
                 imm = ((inst >> 6) & 0x1) << 4;
                 imm |= ((inst >> 5) & 0x1) << 6;
@@ -575,77 +535,66 @@ c1(uint8_t *PC, uint32_t *X, uint16_t inst, uint8_t MODE) {
             break;
         case 4:
             uint8_t microop = (inst >> 10) & 0x7;
+            imm = (inst >> 2) & 0x1f;
+            uint8_t shamt = imm;
+            rd = ((inst >> 7) & 0x7) + 8;
             if (microop == 0x0) { // c.srli
-                uint8_t rd8 = (inst >> 7) & 0x7;
-                uint8_t shamt = (inst >> 2) & 0x1f;
                 if (MODE)
                     evm_print("c.srli x[%d] = x[%d] >>u %d // %x -> %x\n",
-                        8 + rd8, 8 + rd8, shamt, X8[rd8], X8[rd8] >> shamt);
+                        rd, rd, shamt, X[rd], X[rd] >> shamt);
                 if (MODE == EVM_MODE_DISASM)
                     break;
-                X8[rd8] = X8[rd8] >> shamt;
+                X[rd] = X[rd] >> shamt;
             } else if (microop == 0x1) { // c.srai
-                uint8_t rd8 = (inst >> 7) & 0x7;
-                uint8_t shamt = (inst >> 2) & 0x1f;
                 if (MODE)
                     evm_print("c.srai x[%d] = x[%d] >>s %d\n",
-                        8 + rd8, 8 + rd8, shamt);
+                        rd, rd, shamt);
                 if (MODE == EVM_MODE_DISASM)
                     break;
-                int32_t v = X8[rd8];
-                X8[rd8] = v >> shamt;
+                int32_t v = X[rd];
+                X[rd] = v >> shamt;
             } else if (microop == 0x2) { // c.andi
-                uint8_t rd8 = (inst >> 7) & 0x7;
-                uint32_t imm = (inst >> 2) & 0x1f;
                 if (MODE)
                     evm_print("c.andi x[%d] = x[%d] & sext(0x%x)\n",
-                        8 + rd8, 8 + rd8, imm);
+                        rd, rd, imm);
                 if (MODE == EVM_MODE_DISASM)
                     break;
-                X8[rd8] = X8[rd8] & imm;
+                X[rd] = X[rd] & imm;
             } else if (microop == 0x6) { // c.andi
-                uint8_t rd8 = (inst >> 7) & 0x7;
-                uint32_t imm = (inst >> 2) & 0x1f;
-                if (inst & 0x1000)
-                    imm |= 0xfffffff0;
-                X8[rd8] = X8[rd8] & imm;
+                if (MODE)
+                    evm_print("c.andi x[%d] = x[%d] & sext(0x%x)\n",
+                        rd, rd, imm);
+                if (MODE == EVM_MODE_DISASM)
+                    break;
+                imm |= 0xffffffe0;
+                X[rd] = X[rd] & imm;
             } else if (microop == 0x3) {
                 uint8_t mmop = (inst >> 5) & 0x3;
-                uint8_t rd8 = (inst >> 7) & 0x7;
-                uint8_t rs28 = (inst >> 2) & 0x7;
-                if (mmop == 0x0) { // c.sub
-                    if (MODE)
-                        evm_print("c.sub x[%d] = x[%d] - x[%d] // %d - %d\n",
-                            8 + rd8, 8 + rd8, 8 + rs28,
-                            X8[rd8], X8[rs28]);
-                    if (MODE == EVM_MODE_DISASM)
-                        break;
-                    X8[rd8] = X8[rd8] - X8[rs28];
-                } else if (mmop == 0x1) { // c.xor
-                    if (MODE)
-                        evm_print("c.xor x[%d] = x[%d] ^ x[%d]\n",
-                            8 + rd8, 8 + rd8, 8 + rs28);
-                    if (MODE == EVM_MODE_DISASM)
-                        break;
-                    X8[rd8] = X8[rd8] ^ X8[rs28];
-                } else if (mmop == 0x2) { // c.or
-                    if (MODE)
-                        evm_print("c.or x[%d] = x[%d] | x[%d]\n",
-                            8 + rd8, 8 + rd8, 8 + rs28);
-                    if (MODE == EVM_MODE_DISASM)
-                        break;
-                    X8[rd8] = X8[rd8] | X8[rs28];
-                } else if (mmop == 0x3) { // c.and
-                    if (MODE)
-                        evm_print("c.and x[%d] = x[%d] & x[%d] // %x & %x\n",
-                            8 + rd8, 8 + rd8, 8 + rs28, X8[rd8], X8[rs28]);
-                    if (MODE == EVM_MODE_DISASM)
-                        break;
-                    X8[rd8] = X8[rd8] & X8[rs28];
-                }
+                uint8_t rs2 = ((inst >> 2) & 0x7) + 8;
+                if (MODE && mmop == 0x0)
+                    evm_print("c.sub x[%d] = x[%d] - x[%d] // %d - %d\n",
+                        rd, rd, rs2, X[rd], X[rs2]);
+                if (MODE && mmop == 0x1)
+                    evm_print("c.xor x[%d] = x[%d] ^ x[%d]\n", rd, rd, rs2);
+                if (MODE && mmop == 0x2)
+                    evm_print("c.or x[%d] = x[%d] | x[%d]\n", rd, rd, rs2);
+                if (MODE && mmop == 0x3)
+                    evm_print("c.and x[%d] = x[%d] & x[%d] // %x & %x\n",
+                        rd, rd, rs2, X[rd], X[rs2]);
+                if (MODE == EVM_MODE_DISASM)
+                    break;
+                if (mmop == 0x0) // c.sub
+                    X[rd] = X[rd] - X[rs2];
+                if (mmop == 0x1) // c.xor
+                    X[rd] = X[rd] ^ X[rs2];
+                if (mmop == 0x2) // c.or
+                    X[rd] = X[rd] | X[rs2];
+                if (mmop == 0x3) // c.and
+                    X[rd] = X[rd] & X[rs2];
             }
             break;
-        case 5: // c.j
+        case 1: // c.jal imm[11|4|9:8|10|6|7|3:1|5]
+        case 5: // c.j imm[11|4|9:8|10|6|7|3:1|5]
             imm = ((inst >> 2) & 0x1) << 5;
             imm |= ((inst >> 3) & 0x7) << 1;
             imm |= ((inst >> 6) & 0x1) << 7;
@@ -655,41 +604,42 @@ c1(uint8_t *PC, uint32_t *X, uint16_t inst, uint8_t MODE) {
             imm |= ((inst >> 11) & 0x1) << 4;
             if (inst & 0x1000)
                 imm |= 0xfffff800;
-            if (MODE)
+            if (MODE && opcode == 1)
+                evm_print("c.jal x[1] = pc+2; pc += sext(%d)\n",
+                    (int32_t)imm);
+            if (MODE && opcode == 5)
                 evm_print("c.j pc += sext(%d)\n", imm);
             if (MODE == EVM_MODE_DISASM)
                 break;
+            if (opcode == 1)
+                X[1] = (uint32_t) PC + 2;
             return PC + imm;
         case 6: // c.beqz
-            rs18 = (inst >> 7) & 0x7;
-            offset = ((inst >> 3) & 0x3) << 1;
-            offset |= ((inst >> 10) & 0x3) << 3;
-            offset |= ((inst >> 2) & 0x1) << 5;
-            offset |= ((inst >> 5) & 0x3) << 6;
-            if (inst & 0x1000)
-                offset |= 0xffffff00;
-            if (MODE)
-                evm_print("c.beqz if (x[%d] == 0) pc += sext(%d)\n",
-                    8 + rs18, offset);
-            if (MODE == EVM_MODE_DISASM)
-                break;
-            if (X8[rs18] == 0)
-                return PC + offset;
-            break;
         case 7: // c.bnez
-            rs18 = (inst >> 7) & 0x7;
+            rs1 = ((inst >> 7) & 0x7) + 8;
             offset = ((inst >> 3) & 0x3) << 1;
             offset |= ((inst >> 10) & 0x3) << 3;
             offset |= ((inst >> 2) & 0x1) << 5;
             offset |= ((inst >> 5) & 0x3) << 6;
             if (inst & 0x1000)
                 offset |= 0xffffff00;
-            if (MODE)
+            if (MODE && (opcode & 1) == 0)
+                evm_print("c.beqz if (x[%d] == 0) pc += sext(%d)\n",
+                    rs1, offset);
+            if (MODE && (opcode & 1))
                 evm_print("c.bnez if (x[%d] != 0) pc += sext(%d)\n",
-                    8 + rs18, offset);
+                    rs1, offset);
             if (MODE == EVM_MODE_DISASM)
                 break;
-            if (X8[rs18] != 0)
+            int eqz = (X[rs1] == 0);
+            // For documentation
+            // 1 && 0 == 0
+            // if (eqz && (opcode & 1) == 0)
+            //     return PC + offset;
+            // !0 && 1
+            // if (!eqz && (opcode & 1)) //
+            //     return PC + offset;
+            if (eqz ^ (opcode & 1))
                 return PC + offset;
             break;
         default:
@@ -703,12 +653,13 @@ c1(uint8_t *PC, uint32_t *X, uint16_t inst, uint8_t MODE) {
 static uint8_t *
 c2(uint8_t *PC, uint32_t *X, uint16_t inst, uint8_t MODE) {
     const uint8_t opcode = inst >> 13;
-    uint8_t rd;
+    uint8_t rd = (inst >> 7) & 0x1f;
+    uint8_t rs1 = rd;
+    uint8_t rs2 = (inst >> 2) & 0x1f;
     uint8_t *mem;
     uint32_t imm;
     switch (opcode) {
         case 0: // c.slli
-            rd = (inst >> 7) & 0x1f;
             uint8_t shamt = (inst >> 2) & 0x1f;
             if (MODE)
                 evm_print("c.slli x[%d] = x[%d] << %d\n", rd, shamt);
@@ -717,7 +668,6 @@ c2(uint8_t *PC, uint32_t *X, uint16_t inst, uint8_t MODE) {
             X[rd] = X[rd] << shamt;
             break;
         case 2: // c.lwsp
-            rd = (inst >> 7) & 0x1f;
             imm = ((inst >> 2) & 0x3) << 6;
             imm |= ((inst >> 4) & 0x7) << 2;
             imm |= ((inst >> 12) & 0x1) << 5;
@@ -733,16 +683,13 @@ c2(uint8_t *PC, uint32_t *X, uint16_t inst, uint8_t MODE) {
             break;
         case 4:
             if ((inst & 0x1000) == 0) {
-                uint8_t rs2 = (inst >> 2) & 0x1f;
                 if (rs2) { // c.mv
-                    uint8_t rd = (inst >> 7) & 0x1f;
                     if (MODE)
                         evm_print("c.mv x[%d] = x[%d]\n", rd, rs2);
                     if (MODE == EVM_MODE_DISASM)
                         break;
                     X[rd] = X[rs2];
                 } else { // c.jr
-                    uint8_t rs1 = (inst >> 7) & 0x1f;
                     if (MODE)
                         evm_print("c.jr pc = x[%d] // 0x%x\n", rs1, X[rs1]);
                     if (MODE == EVM_MODE_DISASM)
@@ -750,8 +697,6 @@ c2(uint8_t *PC, uint32_t *X, uint16_t inst, uint8_t MODE) {
                     return (uint8_t *) X[rs1];
                 }
             } else {
-                uint8_t rs2 = (inst >> 2) & 0x1f;
-                uint8_t rs1 = (inst >> 7) & 0x1f;
                 if (rs2 == 0) {
                     if (rs1 == 0) { // c.ebreak
                         if (MODE)
@@ -781,7 +726,6 @@ c2(uint8_t *PC, uint32_t *X, uint16_t inst, uint8_t MODE) {
             }
             break;
         case 6: // c.swsp
-            uint32_t rs2 = (inst >> 2) & 0x1f;
             imm = ((inst >> 9) & 0xf) | ((inst >> 7) & 0x3) << 4;
             imm = imm << 2;
             if (MODE)
@@ -815,8 +759,6 @@ evm_interpreter(uint8_t *prog_start, uint8_t *end,
     X[0] = 0; // always
     X[1] = (uint32_t) end; // dummy return point
     X[2] = (uint32_t) &stack[sizeof(stack)]; // stack pointer
-    if (MODE)
-        evm_print("end: 0x%x\n", X[2]);
     X[10] = args->a0;
     X[11] = args->a1;
     X[12] = args->a2;
@@ -828,7 +770,7 @@ evm_interpreter(uint8_t *prog_start, uint8_t *end,
         const uint32_t *ptr = (uint32_t *) PC;
         const uint8_t prefix = *ptr & 0x3;
         if (prefix == 0x3) {
-            const uint8_t opcode = (*ptr >> 2) & 0x1f; // 7 bit
+            const uint8_t opcode = (*ptr >> 2) & 0x1f; // 5 bit
             if (MODE)
                 evm_print("%4x (x%02x.%x) | ", PC - prog_start, opcode, 3);
             switch (opcode) {
